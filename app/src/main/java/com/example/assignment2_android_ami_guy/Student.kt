e06e731b334b4a347f63de2ccda61f10530c0e5d
Ami Kahlon <amikahlon2@gmail.com>
2025-01-23 15:36:41 +0200

    add new student, students repository, student class

---
+++ app/src/main/java/com/example/assignment2_android_ami_guy/Student.kt
@@ -0,0 +1,9 @@
+package com.example.assignment2_android_ami_guy
+
+data class Student(
+    var id: String,
+    var name: String,
+    var phone: String,
+    var address: String,
+    var isChecked: Boolean = false
+)
